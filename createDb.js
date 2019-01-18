8888e5b44d4e2df0e9b4dff06558d60e3631d050
Altyn777 <darzeeia@gmail.com>
2019-01-18 19:08:59 +0200

    useNewUrlParser

--- createDb.js
+++ createDb.js
@@ -2,14 +2,16 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017/chat';
 
 // Database Name
 const dbName = 'myproject';
 
-// Use connect method to connect to the server
-MongoClient.connect.useNewUrlParser = true;
-MongoClient.connect(url, function(err, client) {
+// Create a new MongoClient
+const client = new MongoClient(url, {useNewUrlParser: true});
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
